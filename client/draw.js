d03993fce1182963649bf69f989c35f742892079
Anurag K <anuragkamas1991@gmail.com>
2020-05-02 16:23:06 -0700

    Added text input and button. Emitting and receiving ready events.

--- client/draw.js
+++ client/draw.js
@@ -5,6 +5,7 @@ const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 const socket = io();
 var snake;
+var allPlayersReady = false;
 
 (function setup() {
   snake = new Snake();
@@ -12,20 +13,30 @@ var snake;
   fruit.pickLocation();
 
   window.setInterval(() => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    fruit.draw();
-    snake.update();
-    snake.draw();
+    if (allPlayersReady) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      fruit.draw();
+      snake.update();
+      snake.draw();
 
-    if (snake.eat(fruit)) {
-      fruit.pickLocation();
+      if (snake.eat(fruit)) {
+        fruit.pickLocation();
+      }
+
+      snake.checkCollision();
+      document.querySelector('.score')
+        .innerText = "Score: " + snake.total;
     }
 
-    snake.checkCollision();
-    document.querySelector('.score')
-      .innerText = "Score: " + snake.total;
   }, 250);
 })();
+
+const name = document.querySelector("#name");
+const readyButton = document.querySelector("#readyButton");
+readyButton.addEventListener("click", function() {
+  socket.emit("ready", name.value);
+  console.log("EMITTING READY MESSAGE FOR USER: " + name.value);
+});
 
 window.addEventListener("keydown", (evt) => {
   const direction = evt.key.replace("Arrow", "");
@@ -39,4 +50,9 @@ socket.on("move", function (direction) {
     currentDirection = direction;
     snake.changeDirection(direction);
   }
+});
+
+socket.on("start", function() {
+  console.log("RECEIVED START MESSAGE FROM SERVER");
+  allPlayersReady = true;
 });
